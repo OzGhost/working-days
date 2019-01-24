3268c4a98c9330b058479e37beba0cd01784ee3c
nhduc <duc.nguyenhoang@axonactive.com>
2019-01-24 15:48:32 +0700

    turn off debug mode

--- bg.js
+++ bg.js
@@ -493,9 +493,9 @@
   var b = document.createElement('BUTTON')
   b.appendChild(document.createTextNode('Knock'));
   b.addEventListener('click', function(){
-    //_this.visit();
-    //document.body.removeChild(b);
-    _this.calculateIssue(b, 'AF-24734');
+    _this.visit();
+    document.body.removeChild(b);
+    //_this.calculateIssue(b, 'AF-24734');
   });
   _this.pin(b, 0);
 })();
